ae86121e091c713519665ef869b3a196becfc4b6
Iveta <iveta.nagled@gmail.com>
2021-02-02 20:23:26 +0200

    Initial commit

---
+++ src/main/scala/MyApp.scala
@@ -0,0 +1,6 @@
+object MyApp extends App{
+  println("My first Scala program!")
+  print("Print has no new line ")
+  print("it can be seen here")
+
+}
---
+++ src/main/scala/hello.sc
@@ -0,0 +1,4 @@
+println("Hello there Scala fans!")
+//experimenting worksheet
+val a = 5+6
+println(a)
