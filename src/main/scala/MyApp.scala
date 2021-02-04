410b4027936a425c7a58f01170540eeaa76d855e
Iveta <iveta.nagled@gmail.com>
2021-02-04 20:02:06 +0200

    Food buying App

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,3 +1,5 @@
+import scala.io.StdIn.readLine
+
 object MyApp extends App{
   println("My first Scala program!")
   print("Print has no new line ")
@@ -5,4 +7,15 @@ object MyApp extends App{
 
   println("Continue:")
   println("Adding something more to my code")
+
+
+  val myName = readLine("What is your name friend?") //readline will return Stringgs
+  val favoriteFood = readLine(s"Hello there $myName what is your favorite food?")
+  println(s"Cool I like $favoriteFood as well!" )
+  val currentPrice = readLine(s"How much $favoriteFood is/are in stores right now?").toFloat
+  println(s"Not bad I could buy some $favoriteFood for $currentPrice")
+  val currentMoney = readLine(s"How much money do you have in your wallet?").toFloat
+  //TODO find out how much we cab
+  val maxPurchaseWeight = currentMoney / currentPrice
+  println(s"Congratulations $myName! You can buy ${(maxPurchaseWeight*1000).round/1000.0} kilos of $favoriteFood right now")
 }
