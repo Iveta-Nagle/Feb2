9f656fc1edbe8f4a071d906ed29b7b1c2fd40c50
Iveta <iveta.nagled@gmail.com>
2021-02-09 21:51:51 +0200

    Added some yield examples

--- src/main/scala/FizzBuzzExercise.scala
+++ src/main/scala/FizzBuzzExercise.scala
@@ -16,4 +16,39 @@ object FizzBuzzExercise extends App {
     else println(i)
   }
 
+  //we used a for loop to create a collection of values
+  val mySeq = for {
+    i <- 11 to 20
+  } yield i
+  mySeq.foreach(println)
+
+  val myNumbers = (21 to 30).toIndexedSeq
+  println(myNumbers(4))
+
+
+  //indexes in programming start at 0
+  println(mySeq(0))
+  println(mySeq(3)) // this prints 14
+
+
+  //very rarely used in Scala and not recommended and too wordy and more chance to mess up
+  for (i <-0  until myNumbers.length) {
+    println(myNumbers(i))
+  }
+
+  //if we need index in Scala we can use zipWithIndex which creates and number
+  for ((value, index) <- myNumbers.zipWithIndex) {
+    println(s"Index $index value: $value ")
+  }
+
+  //we can create a sequence of FizzBuzz items for future use
+  val myFizzBuzz = for {
+    i <- 1 to 40
+  } yield if (i % 5 == 0 && i % 7 == 0) "FizzBuzz"
+  else if (i % 5 == 0) "Fizz"
+  else if (i % 7 == 0) "Buzz"
+  else i.toString //best to keep all items same type to have less any which loose
+
+  myFizzBuzz.foreach(println)
+
 }
