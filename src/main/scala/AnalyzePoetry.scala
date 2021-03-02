08393cce50f56d10c5b25033028612c39488460a
Iveta <iveta.nagled@gmail.com>
2021-03-02 22:04:30 +0200

    Regex

--- src/main/scala/AnalyzePoetry.scala
+++ src/main/scala/AnalyzePoetry.scala
@@ -1,4 +1,5 @@
 import AnalyzePoetry.lines
+import Maps.args
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -112,4 +113,79 @@ object AnalyzePoetry extends App {
   println("*"*40)
   noSubTitles.foreach(println)
 
+  def extractUnderscoreText(s: String) :String = s match {
+    case s"${head}_${target}_$tail" => s"HEAD: $head, TARGET: $target, TAIL: $tail"
+    case _=> "no match"
+  }
+
+  val underscoreLines = lines.filter(line => extractUnderscoreText(line) != "no match")
+  underscoreLines.foreach(println)
+
+  //how about transforming data first? and filter later?
+ val transformedText = lines.map(extractUnderscoreText)
+
+  //transformedText.foreach(println)
+
+  val transformedFilteredText = transformedText.filter(_ != "no match")
+  transformedFilteredText.foreach(println)
+
+  def getAuthorsTitles(lines: Array[String]): Unit = {
+    var emptyCount = 0
+    val authorCount = 3
+    val titleCount = 4
+    //idea we count empty lines and reset counter upon first non empty
+    //if this non empty line has uppercase we can decide on whether that is author or title
+    for (line <- lines) {
+      if (line.trim.length == 0) emptyCount +=1
+      else if (line.toUpperCase == line) {
+        if (emptyCount == authorCount) println(s"AUTHOR: $line")
+        else if (emptyCount == titleCount) println(s"TITLE: $line")
+        emptyCount = 0
+      } else emptyCount = 0
+    }
+  }
+
+  getAuthorsTitles(lines)
+
+  //start 340 end 4488
+
+  //TODO get only poem text only use starting 340 and ending 4488
+  //If feeling comfortable, add also capital author and title
+
+  def getPoemText(lines: Array[String]): Array[String] = {
+    lines.filter(line => !(line.toUpperCase == line) || line.length == 0)
+  }
+
+  val poemTextOnly = getPoemText(lines.slice(340,4488))
+  poemTextOnly.foreach(println)
+
+  val onlyPageNumbers = lines.filter(line => line.matches((""".+_\d+_""")))
+  onlyPageNumbers.foreach(println)
+
+  //now lets extract title and the page number exactly
+
+  val regEx = """(.+)_(\d+)_""".r
+  val poemPagesProcessed = for (line <- onlyPageNumbers) yield {
+    val regEx(title, page) = line
+    (title, page)
+  }
+
+  poemPagesProcessed.foreach(println)
+ val poemPagesTrimmed = poemPagesProcessed.map(myTuple => (myTuple._1.trim, myTuple._2.trim))
+  poemPagesTrimmed.foreach(println)
+
+  val dedications = lines.filter(line => line.matches(".*\\(.+\\).*"))
+  dedications.foreach(println)
+
+  //we could get a more universal regex for phone extraction here
+  //https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
+  val phoneNumbers = lines.filter(line => line.matches(".*\\d{3}.{0,2}\\d{3}.{0,2}\\d{4}.*"))
+  println(s"Got ${phoneNumbers.length} lines with phone numbers")
+  phoneNumbers.foreach(println)
+  val justPhoneRegEx = """.*(.\d{3}.{0,2}\d{3}.{0,2}\d{4}).*""".r
+  val phones = phoneNumbers.map(line => {
+    val justPhoneRegEx(phone) = line //a bit of strange syntax but it creates the results in phone from line given your regex
+    phone
+  })
+  phones.foreach(println)
 }
