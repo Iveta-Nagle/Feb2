4fff0ede9e990f7c629d18441bdedbdd9f7e10ef
Iveta <iveta.nagled@gmail.com>
2021-03-08 22:21:52 +0200

    Recipes

--- src/main/scala/CookbookExercise.scala
+++ src/main/scala/CookbookExercise.scala
@@ -1,3 +1,5 @@
+import java.io.FileWriter
+
 import ReadingFiles.and_text
 
 import scala.collection.mutable.ArrayBuffer
@@ -12,32 +14,8 @@ object CookbookExercise extends App {
     lines
   }
 
-  def saveLines(lines: Array[String], destPath: String, sep: String = "\n") = {
-    val txt = lines.mkString(sep)
-    import java.io.{PrintWriter, File}
-    val pw = new PrintWriter(new File(destPath))
-    pw.write(txt)
-    pw.close
-  }
-
-
-
-//  val relative_save_path = "/Users/ivetanagle/IdeaProjects/Feb2/src/resources/13177-8_cleaned.txt"
-//  import java.io.{PrintWriter, File}
-//  val pw = new PrintWriter(new File(relative_save_path))
-//  pw.write(and_text)
-//  pw.close
-
   val srcName = "/Users/ivetanagle/IdeaProjects/Feb2/src/resources/13177-8.txt"
   val dstName = "/Users/ivetanagle/IdeaProjects/Feb2/src/resources/13177-8-cleaned.txt"
-
-  def openSource(fName:String) = {
-    val filePointer = scala.io.Source.fromFile(srcName)
-    val myLines = filePointer.getLines.toSeq
-    filePointer.close()
-    myLines
-  }
-
 
 
   val cookBook_lines = getLinesFromFile(srcName)
@@ -46,9 +24,6 @@ object CookbookExercise extends App {
   val startLine = Utilities.findNeedle(cookBook_lines, needle)
   val onlyRecipeNames = cookBook_lines.slice(startLine,4324).filter(line => line.matches(""".+\w"""))
   val recipes= onlyRecipeNames.map(line => line.toUpperCase)
-  recipes.foreach(println)
-
-
 
   def processSeq(mySeq:Seq[String], recipes:Array[String])= {
 
@@ -59,48 +34,30 @@ object CookbookExercise extends App {
         curRecipe = line
       } else if (line.length > 2  && line.matches("""\s+\d.+""")) {
         val ingredients = recipeMap(curRecipe)
-        //ingredients += line
+        ingredients += line
       }
     }
     val results = for ((recipe, ingredients) <- recipeMap) yield (recipe, ingredients.toArray)
     results
-//    //iterative solution
-//    //TODO filter by multiple lines meaning CAPITAL then possible Illustration then 4 spaces
-//    val ingredients = mySeq.filter(line => line.startsWith("    ")) //TODO add titles possibly with regex
-//    //TODO more filtering for Illustrations and of course false titles meaning titles which do not have ingredients following
-//    recipes
   }
 
+  def saveSeq(destName:String, mySeq:Map[String, Array[String]], sep: String = "\n") = {
+    val fw = new FileWriter(dstName)
+    for ((key, value ) <- mySeq) {
+      fw.write(key + sep + sep)
+      value.foreach(ingredient => fw.write(ingredient + sep))
+      fw.write(sep)
+    }
+    fw.close()
+  }
 
   val filteredSeq = processSeq(cookBook_lines.slice(96,4076), recipes)
+  saveSeq(dstName,filteredSeq)
   for ((key, value ) <- filteredSeq) {
     println(s"Recipe $key")
     println("Ingredients")
-    value.foreach(println)
     value.foreach(ingredient => ingredient.split("\\n+").foreach(println))
   }
-
-//  val recipeMap = recipes.map( recipe => (recipe, ArrayBuffer[String]())).toMap
-//  for ((key, value ) <- recipeMap) {
-//    println(s"R $key")
-//    println("I")
-//    value.foreach(println)
-//    value.foreach(ingredient => ingredient.split("\\n+").foreach(println))
-//  }
-
-//  def saveSeq(destName:String, mySeq:Seq[String]) = {
-//    println(s"Saving my Sequence to file $destName")
-//    //    mySeq.foreach(println) //we are good up to here
-//    val fw = new FileWriter(dstName)
-//    mySeq.map(_ + "\n").foreach(fw.write) // adding new line to each line before writing
-//    fw.close()
-//  }
-//
-//  //the actual program runs here, little tiny pipeline like a factory
-//  val mySeq = openSource(srcName)
-//  val filteredSeq = processSeq(mySeq)
-//  saveSeq(dstName,filteredSeq)
-
 
 
 }
